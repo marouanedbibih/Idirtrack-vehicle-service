44b8bdd207357e8b7c332a105187630ce168ac47
marouanedbibih <dbibih.marouane@emsi-edu.ma>
2024-07-08 12:43:01 +0100

    First commit

---
+++ src/test/java/com/idirtrack/vehicle_service/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idirtrack.vehicle_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
